ae00f201e4964ade7d3dfcfa06482b800ac64414
Stanley Lalanne <stanleylalanne@cs-c09-22.mec.science>
2019-12-03 11:57:41 -0500

    finalizing client

--- MovieLabelGraph/LabelGraphClient.cpp
+++ MovieLabelGraph/LabelGraphClient.cpp
@@ -5,7 +5,27 @@ int main() {
   using namespace std;
   LabeledGraph slalanneLabelGraph("movies_short.txt");
 
+//take input queries from users
 
-cout<<slalanneLabelGraph;
+bool play = true;
+
+/*
+while(play){
+
+  cout<<"Enter the name of an actor or a movie:\n";
+  cout<<"Enter STOP to stop the program.\n";
+
+  std::string query = "";
+  cin>> query;
+  if (query == "STOP" || query == "stop") play = false;
+
+  cout<<"Here are neighbors of "<<query<<":\n";
+  slalanneLabelGraph.print_edges(query);
+
+}
+
+*/
+
+slalanneLabelGraph.print_edges("Braid, Hilda");
  
 }
